52c267e62d9838b49877592bdab31057188c90a7
akramLh005 <akram.lahmar@insat.ucar.tn>
2023-11-19 22:48:59 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { CvComponent } from './cvTech/cv/cv.component';
+import { ListCvComponent } from './cvTech/list-cv/list-cv.component';
+import { ItemComponent } from './cvTech/item/item.component';
+import { DetailCvComponent } from './cvTech/detail-cv/detail-cv.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CvComponent,
+    ListCvComponent,
+    ItemComponent,
+    DetailCvComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cvTech/detail-cv/detail-cv.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-cv',
+  templateUrl: './detail-cv.component.html',
+  styleUrls: ['./detail-cv.component.css']
+})
+export class DetailCvComponent {
+
+}
